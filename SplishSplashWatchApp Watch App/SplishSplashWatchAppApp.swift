a60367dc9ea578e6dc6d6ed1c0dfbc1da5df8e58
kangddong <rkdehddud96@naver.com>
2025-08-05 23:17:07 +0900

    [ADD] Watch App 추가

---
+++ SplishSplashWatchApp Watch App/SplishSplashWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplishSplashWatchAppApp.swift
+//  SplishSplashWatchApp Watch App
+//
+//  Created by 강동영 on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SplishSplashWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
