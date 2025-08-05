2be5495336b29c41a38619d7261e4d5cf2b5277f
kangddong <rkdehddud96@naver.com>
2025-08-05 22:53:12 +0900

    Initial Commit

---
+++ SplishSplash/SplishSplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplishSplashApp.swift
+//  SplishSplash
+//
+//  Created by 강동영 on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SplishSplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
